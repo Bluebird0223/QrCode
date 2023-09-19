9804e58ea33dbb3b37fbc12a735c848b537748b0
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2023-09-19 14:41:41 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import QRCodeScanner from './QRCodeScanner'
+import React, { useEffect, useState } from 'react';
+import { Html5QrcodeScanner } from 'html5-qrcode';
 
 
 function App() {
+
+  const[scanResult,setScanResult]=useState(null) 
+  useEffect(()=>{
+
+    
+  const scanner=new Html5QrcodeScanner('reader',{
+    qrbox:{
+      width:250,
+      height:250,
+    },
+    fps:5,
+  })
+  scanner.render(success,error)
+
+  function success(result){
+    scanner.clear()
+    setScanResult(result)
+
+  }
+
+  function error(err){
+    console.log(err);
+  }
+
+
+  },[])
+
   return (
     <div className="App">
-      <QRCodeScanner />
-    </div>
+      <h3>QR code scanner</h3>
+      {
+        scanResult
+        ?<div>Success:<a href={scanResult}>{scanResult}</a></div>
+        :<div id='reader'></div>
+      }
+      </div>
+    
   );
 }
 
